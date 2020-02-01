c9389826bd44e43200ad2d49f2551ebea046f9eb
Joell <amamihechi.udu@decagon.dev>
2020-02-01 06:11:18 +0100

    expressions and conditionals in razor

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,40 +1,32 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 
 using videly.Models;
+using videly.ViewModels;
 
 namespace videly.Controllers {
     public class MoviesController :Controller {
         // GET: Movies/Random
         public ActionResult Random() {
 
-            var movies = new Movie {
+            var movie = new Movie {
                 Name = "Iron Man" ,
                 Description = "Since time began, Cryptionites has been Iron Man's worst enemy and his enemies best friend. This has been over exploited in the past to a great extent."
                 };
-            return View(movies);
+
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel {
+                Movie = movie ,
+                Customers = customers
+                };
+            return View(viewModel);
 
             }
 
-        public ActionResult Edit(int moviesId) {
-
-
-            return Content($"id = {moviesId}");
-            }
-
-        public ActionResult Index(int? pageIndex , string sortby) {
-
-            if (!pageIndex.HasValue) {
-                pageIndex = 1;
-                }
-
-            if (string.IsNullOrWhiteSpace(sortby)) {
-                sortby = "Name";
-                }
-            return Content("Welcome");
-            }
-
-        public ActionResult ByReleaseDate(int year , int month) {
-            return Content($"Year: {year}\tMonth: {month}");
-            }
         }
     }
---
+++ Models/Customer.cs
@@ -0,0 +1,7 @@
+﻿namespace videly.Models {
+    public class Customer {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        }
+    }
